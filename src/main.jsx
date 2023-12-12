4748f059fc29f702bc14f704bde71307eea27cfd
Sourav Selvaraj <souravselvaraj@gmail.com>
2023-12-12 18:42:07 +0530

    changed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
-export default App;
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
